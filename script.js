09a08757637cc4dfef080af656c14bee4306696e
josephinejenks <61992137+josephinejenks@users.noreply.github.com>
2020-03-16 16:25:38 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,28 +1,28 @@
 var stoppedx = ballvx;
-		var stoppedy = ballvy;
-		var stoppedx2 = ballvx2;
-		var stoppedy2 = ballvy2;
-		var boxx = 0; // x coordinate for upper left corner of box
-		var boxy = 0; // y coordinate for upper left corner of box
-		var boxwidth = 550; 
-		var boxheight = 700;
-		var ballrad = 30;
-		var boxboundx = boxwidth+boxx-ballrad; // right boundary
-		var boxboundy = boxheight+boxy-2*ballrad; // bottom boundary
-		var inboxboundx = boxx+ballrad; // left boundary
-		var inboxboundy = boxy+2*ballrad; // top boundary
-		var ballx = inboxboundx; // initial x position of ball
-		var bally = 415; // initial y position of ball
-		var ballvx = 2.5; // horizontal displacement
-		var ballvy = -0.80; // vertical displacement
-		var ballx2 = boxboundx; // initial x position of ball2
-		var bally2 = 265; // initial y position of ball
-		var ballvx2 = -2.5; // horizontal displacement
-		var ballvy2 = 0.80; // vertical displacement
-		var ball = new Image();
-		var ball2 = new Image();
-		var bkg = new Image();
-		var ctx;
+var stoppedy = ballvy;
+var stoppedx2 = ballvx2;
+var stoppedy2 = ballvy2;
+var boxx = 0; // x coordinate for upper left corner of box
+var boxy = 0; // y coordinate for upper left corner of box
+var boxwidth = 550; 
+var boxheight = 700;
+var ballrad = 30;
+var boxboundx = boxwidth+boxx-ballrad; // right boundary
+var boxboundy = boxheight+boxy-2*ballrad; // bottom boundary
+var inboxboundx = boxx+ballrad; // left boundary
+var inboxboundy = boxy+2*ballrad; // top boundary
+var ballx = inboxboundx; // initial x position of ball
+var bally = 415; // initial y position of ball
+var ballvx = 2.5; // horizontal displacement
+var ballvy = -0.80; // vertical displacement
+var ballx2 = boxboundx; // initial x position of ball2
+var bally2 = 265; // initial y position of ball
+var ballvx2 = -2.5; // horizontal displacement
+var ballvy2 = 0.80; // vertical displacement
+var ball = new Image();
+var ball2 = new Image();
+var bkg = new Image();
+var ctx;
 		/* var grad;
 		var color;
 		var hue = [
@@ -46,8 +46,22 @@ var stoppedx = ballvx;
 				color = 'rgb('+hue[h][0]+','+hue[h][1]+','+hue[h][2]+')';
 				grad.addColorStop(h*1/hue.length,color);
 			} */
+			
+			changeImage();
+			tidTwo = setInterval(changeImage, 23000);
+
 			moveballs();
 			tidOne = setInterval(moveballs,100);
+		}
+
+		function changeImage() {
+			if (bkg.src.match("background2.jpg")) {
+				bkg.src = "background1.jpg";
+			}
+			else {
+				bkg.src="background2.jpg";
+			}
+			ctx.drawImage(bkg,boxx+2*ballrad,boxy+2*ballrad,boxwidth-4*ballrad,boxheight-4*ballrad);
 		}
 
 		/*function changebkg() {
